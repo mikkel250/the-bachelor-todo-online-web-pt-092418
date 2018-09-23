26d06640b6ba5c61ad15896cd4f7257b81c589f9
Mikkel Ridley <mikkel250@gmail.com>
2018-09-23 05:46:44 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,32 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_name = "temp"
+  data.each do |seasons, contestant_array|
+  	# each season is a key (also a symbol) and each value is an array with a hash inside it
+
+  	#binding.pry
+  	if seasons == season  
+			contestant_array.each do |hashes| 	#this is an array filled with hashes
+				hashes.each do |stats, strings|    #each hash has all the stats of each contestant as k (symbols), v (strings) pairs. The keys are symbols
+  				     
+	  				if hashes[:status] == "Winner"
+
+				      winner_name = hashes[:name].split[0]
+	  					winner_name_split = winner_name.split
+	  					return winner_name_split[0]
+  					   # need to split and return only first name
+  				end
+  			end
+  		
+  		end
+  		
+  	end
+  end
+  #winner_name.split[0]
+ # binding.pry
 end
+
 
 def get_contestant_name(data, occupation)
   # code here
