8f0fb9304d10bc22f28d709a9b733d3bc76b05b9
Mikkel Ridley <mikkel250@gmail.com>
2018-09-24 05:36:00 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,7 +39,19 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+ count = 0
+ data.each do |seasons, contestant_array|
+			contestant_array.each do |hashes| 	#this is an array filled with hashes
+				hashes.each do |stats, strings|    
+  				if hashes["hometown"] == hometown 
+
+			      count += 1	  					
+  				end
+  			end  		
+  		end  		
+  	end
+  end 
+  count
 end
 
 def get_occupation(data, hometown)
