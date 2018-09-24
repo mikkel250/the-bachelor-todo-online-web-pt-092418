ac23197144f4130021ef5bc78afc560e81725cd8
Mikkel Ridley <mikkel250@gmail.com>
2018-09-24 05:03:19 +0000

    Done.

---
+++ lib/bachelor.rb
@@ -0,0 +1,51 @@
+
+require 'pry'
+
+def get_first_name_of_season_winner(data, season)
+ 
+  data.each do |seasons, contestant_array|
+  	# each season is a key (also a symbol) and each value is an array with a hash inside it
+
+  	#binding.pry
+  	if seasons == season  
+			contestant_array.each do |hashes| 	#this is an array filled with hashes
+				hashes.each do |stats, strings|    #each hash has all the stats of each contestant as k (symbols), v (strings) pairs. The keys are symbols
+  				     
+	  				if hashes["status"] == "Winner"
+
+				      return hashes["name"].split[0]
+	  					
+  				end
+  			end
+  		
+  		end
+  		
+  	end
+  end
+ 
+end
+
+
+def get_contestant_name(data, occupation)
+	data.each do |seasons, contestant_array|
+			contestant_array.each do |hashes| 	#this is an array filled with hashes
+				hashes.each do |stats, strings|    
+  				if hashes["occupation"] == occupation
+			      return hashes["name"]
+  			end  		
+  		end  		
+  	end
+  end 
+end
+
+def count_contestants_by_hometown(data, hometown)
+  # code here
+end
+
+def get_occupation(data, hometown)
+  # code here
+end
+
+def get_average_age_for_season(data, season)
+  # code here
+end
